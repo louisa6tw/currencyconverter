ccacce0e989b0970b8051a6b0b0749045837da2d
Louisa <130015498+louisa6tw@users.noreply.github.com>
2023-06-11 23:43:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ window.onload = function() { //when page loads
     const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
     document.getElementsByClassName("date")[0].innerHTML = dateString;
 }
+const btn = document.getElementById("convert-button");
+btn.addEventListener("click", function() {
+    var amount = document.getElementById("amount").value;
+    var from = document.getElementById("before").value;
+    var to = document.getElementById("after").value;
+
+    axios.get(`http://localhost:3000/convert?from=${from}&to=${to}&amount=${amount}`)
+    .then(response => {
+        console.log(response.data); // log the response data
+
+            var result = response.data.result;
+            document.getElementById("result").innerText = "Result: " + result + " " + to;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById("result").innerText = "Conversion error";
+        });
+});
